54a251acf7bc1ecd66565473c99cb115a8ac85e7
Paul Oosthuysen <oosthuysen.paul@gmail.com>
2015-12-17 22:40:30 +0200

    combined expenses and incomes as transactions (same query)

--- source/src/transactions/controller.transactions.js
+++ source/src/transactions/controller.transactions.js
@@ -5,33 +5,34 @@
 			$scope.title = 'Transactions';
 			
 			$scope.transactions = [];
-			$scope.transactionsTotal = 0;
+			$scope.expensesTotal = 0;
+			$scope.incomesTotal = 0;
+			$scope.cashflowtotal = 0;
 			
 			$scope.init = init;
-			$scope.loadExpenses = loadExpenses;
-			$scope.loadIncomes = loadIncomes;
 			
 			function init(){
-				loadExpenses();
-				loadIncomes();
+				loadTransactions();
 			}
 			
-			function loadExpenses(){
-				$http.get('data/expenses.json').success(function(response){
+			function loadTransactions(){
+				$http.get('data/transactions.json').success(function(response){
 					for(var i = 0; i < response.length; i++){
-						response[i].Amount = response[i].Amount * -1;
-						$scope.transactions.push(response[i]);
-						$scope.transactionsTotal += parseFloat(response[i].Amount, 2);
+						var type = response[i].Type;
+						
+						if(type === "Income"){
+							$scope.incomesTotal += parseFloat(response[i].Amount, 2);
+						}
+						
+						if(type === "Expense"){
+							response[i].Amount = response[i].Amount * -1;
+							$scope.expensesTotal += parseFloat(response[i].Amount, 2);
+						}
+						
+						$scope.cashflowtotal += parseFloat(response[i].Amount, 2);
 					}
-				});
-			}
-			
-			function loadIncomes(){
-				$http.get('data/incomes.json').success(function(response){
-					for(var i = 0; i < response.length; i++){
-						$scope.transactions.push(response[i]);
-						$scope.transactionsTotal += parseFloat(response[i].Amount, 2);
-					}
+					
+					$scope.transactions = response;
 				});
 			}
 		}
