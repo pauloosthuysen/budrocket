95d58206ef3ce0bc44548ef9756deee88cdd7710
Paul Oosthuysen <oosthuysen.paul@gmail.com>
2015-12-21 20:35:10 +0200

    added tags functionality

--- source/src/home/controller.home.js
+++ source/src/home/controller.home.js
@@ -2,15 +2,19 @@
 	angular.module('BudRocketApp.controllers').controller('HomeCtrl', ['$scope', '$rootScope', '$http',
 		function($scope, $rootScope, $http){
 			$rootScope.CurrentPage = 'Home';
-			$scope.title = 'Home';
+			$scope.title = 'Overview';
 			$scope.expensesTotal = 0;
 			$scope.incomesTotal = 0;
 			$scope.cashflowtotal = 0;
+			$scope.transactions = [];
+			$scope.tags = [];
 			
 			$scope.init = init;
+			$scope.getTagTotal = getTagTotal;
 			
 			function init(){
 				loadTransactions();
+				getTags();
 			}
 			
 			function loadTransactions(){
@@ -28,8 +32,32 @@
 						}
 						
 						$scope.cashflowtotal += parseFloat(response[i].Amount, 2);
+						
+						$scope.transactions.push(response[i]);
 					}
 				});
+			}
+			
+			function getTags(){
+				$http.get('data/tags.json').success(function(response){
+					for(var i = 0; i < response.length; i++){
+						var tag = response[i];
+						
+						$scope.tags.push(tag);
+					}
+				});
+			}
+			
+			function getTagTotal(tagName){
+				var retVal = 0;
+				
+				for(var i = 0; i < $scope.transactions.length; i++){
+					if($scope.transactions[i].Tag == tagName){
+						retVal += parseFloat($scope.transactions[i].Amount, 2);	
+					}
+				}
+				
+				return retVal;
 			}
 		}
 	]);
