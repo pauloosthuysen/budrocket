cca4c76b840f53eecb576894dce3d4c0a419d9eb
Paul Oosthuysen <oosthuysen.paul@gmail.com>
2015-12-21 20:39:24 +0200

    changed 'catgeories' to 'tags'

--- source/src/categories/controller.categories.js
+++ source/src/categories/controller.categories.js
@@ -2,7 +2,7 @@
 	angular.module('BudRocketApp.controllers').controller('CategoriesCtrl', ['$scope', '$rootScope','$http',
 		function($scope, $rootScope, $http){
 			$rootScope.CurrentPage = 'Categories';
-			$scope.title = 'Categories';
+			$scope.title = 'Tags';
 			
 			$scope.init = init;
 			
